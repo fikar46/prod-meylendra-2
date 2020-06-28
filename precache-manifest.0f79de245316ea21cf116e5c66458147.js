2e89131c937f554607c631c6e47150cd0400cc45
M Zulfikar maylendra <zulfikar@doku.com>
2020-06-28 15:15:41 +0700

    first

---
+++ precache-manifest.0f79de245316ea21cf116e5c66458147.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7573065a93b2de205e480e4bb82ca521",
+    "url": "/static/media/icon_2.7573065a.png"
+  },
+  {
+    "revision": "5acf48de28e391d55435",
+    "url": "/static/css/main.681d3636.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "7206087d9288689ce38a",
+    "url": "/static/js/2.2ea7165a.chunk.js"
+  },
+  {
+    "revision": "1235c476d3f0bc80b60761e16fcc0587",
+    "url": "/static/media/paket_1.1235c476.png"
+  },
+  {
+    "revision": "390f6a0546f7726c080dcf1ada2c0604",
+    "url": "/static/media/icon_1.390f6a05.png"
+  },
+  {
+    "revision": "5acf48de28e391d55435",
+    "url": "/static/js/main.190a18c2.chunk.js"
+  },
+  {
+    "revision": "fcf22d29953eac7990eacddb90e87548",
+    "url": "/static/media/book.fcf22d29.png"
+  },
+  {
+    "revision": "4c7906471b09b68fdbfb2027e77a4ca5",
+    "url": "/static/media/consulting.4c790647.png"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "7206087d9288689ce38a",
+    "url": "/static/css/2.e2ae87fc.chunk.css"
+  },
+  {
+    "revision": "91795180487f33cbfc986cdecc44c140",
+    "url": "/index.html"
+  }
+];
